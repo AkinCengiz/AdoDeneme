dbf2ca2fc5df39c028717fcc19c0acfb3f44855e
akincengiz <akincengiz@outlook.com.tr>
2022-11-13 14:01:58 +0300

    Dosyalar yüklendi.

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AdoNetDemo;
+
+namespace AdoNetDemos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void Temizle()
+        {
+            tbxName.Clear();
+            tbxStockAmount.Clear();
+            tbxUnitPrice.Clear();
+            tbxName.Focus();
+        }
+
+        private ProductDal _productDal = new ProductDal();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvProduct.DataSource = _productDal.GetAll();
+        }
+    }
+}
---
+++ ProductDal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDemo
+{
+    public class ProductDal
+    {
+        SqlConnection _connection = new SqlConnection(@"server=(localdb)\mssqllocaldb; initial catalog=ETrade;integrated security=true");
+
+        public List<Product> GetAll()
+        {
+            ConnectionControl();
+            SqlCommand command = new SqlCommand("select * from Products", _connection);
+            SqlDataReader reader = command.ExecuteReader();
+            List<Product> products = new List<Product>();
+            while (reader.Read())
+            {
+                Product product = new Product
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Name = reader["Name"].ToString(),
+                    UnitPrice = Convert.ToDecimal(reader["UnitPrice"]),
+                    StockAmount = Convert.ToInt32(reader["StockAmount"])
+                };
+                products.Add(product);
+            }
+            reader.Close();
+            _connection.Close();
+            return products;
+        }
+
+        public void ConnectionControl()
+        {
+            if (_connection.State == ConnectionState.Closed)
+            {
+                _connection.Open();
+            }
+        }
+    }
+}
